046854b759017c192ca5420fa62696ec633d9f9d
Bruno Donatoni <Brunodonatoni47@gmail.com>
2025-02-02 23:43:00 -0300

    Validation

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -34,6 +34,12 @@ namespace SalesWebMvc.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller ) 
         {
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll();
+                var viewmodel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewmodel);
+            }
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
@@ -100,6 +106,12 @@ namespace SalesWebMvc.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Edit(int id, Seller seller) 
         {
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll();
+                var viewmodel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewmodel);
+            }
             if (id != seller.Id) 
             {
                 return RedirectToAction(nameof(Error), new { message = "Id mismatch" });
--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -9,16 +9,24 @@ namespace SalesWebMvc.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage= "{0} required")]
+        [StringLength(60, MinimumLength =3, ErrorMessage = "{0} size should be between {2} and {1} characters")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "{0} required")]
         [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "Enter a valid {0}")]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "{0} required")]
         [Display(Name = "Bith Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
 
+        [Range(100.0, 50000.0, ErrorMessage ="{0} must be from {1} to {2}")]
+        [Required(ErrorMessage = "{0} required")]
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
