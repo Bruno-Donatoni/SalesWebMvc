bace3bc43ca08f9065e86a422dd2e0cdf49d8a0e
Bruno Donatoni <Brunodonatoni47@gmail.com>
2025-02-03 00:07:01 -0300

    Async operations using Tasks(async, await)

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -25,13 +25,15 @@ namespace SalesWebMvc.Models
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
 
+        [Required(ErrorMessage ="{0} required")]
         [Range(100.0, 50000.0, ErrorMessage ="{0} must be from {1} to {2}")]
-        [Required(ErrorMessage = "{0} required")]
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
+
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
-        public Department Department { get; set; }
+        public Department? Department { get; set; }
+        [Display(Name="Department")]
         public int DepartmentId { get; set; }
 
         public Seller()
--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -16,31 +16,32 @@ namespace SalesWebMvc.Services
             _context = context;
         }
 
-        public List<Seller> FindAll() 
+        public async Task<List<Seller>> FindAllAsync() 
         {
-            return _context.Seller.ToList();
+            return await _context.Seller.ToListAsync();
         }
-        public void Insert(Seller obj) 
+        public async Task InsertAsync(Seller obj) 
         {
             _context.Add(obj);
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
         }
 
-        public Seller FindByID(int id) 
+        public async Task<Seller> FindByIDAsync(int id) 
         {
-            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(seller => seller.Id == id);
+            return await _context.Seller.Include(obj => obj.Department).FirstOrDefaultAsync(seller => seller.Id == id);
         }
 
-        public void RemoveSeller(int id) 
+        public async Task RemoveSellerAsync(int id) 
         {
-            var obj = _context.Seller.Find(id);
+            var obj = await _context.Seller.FindAsync(id);
             _context.Seller.Remove(obj);
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
         }
 
-        public void Update(Seller obj) 
+        public async Task UpdateAsync(Seller obj) 
         {
-            if(!_context.Seller.Any(x => x.Id == obj.Id)) 
+            bool hasAny = await _context.Seller.AnyAsync(x => x.Id == obj.Id);
+            if (!hasAny) 
             {
                 throw new NotFoundException("ID not found!");
             }
@@ -48,7 +49,7 @@ namespace SalesWebMvc.Services
             {
 
                 _context.Update(obj);
-                _context.SaveChanges();
+                await _context.SaveChangesAsync();
             }
             catch (DbUpdateConcurrencyException e) 
             {
