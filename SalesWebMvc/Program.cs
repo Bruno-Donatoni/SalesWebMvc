3bf0016ca42172a516bd9774021c08cf88b5f06a
Bruno Donatoni <Brunodonatoni47@gmail.com>
2025-02-02 22:15:40 -0300

    SellerFomrViewModel and Department select component

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -14,6 +14,7 @@ internal class Program
 
         builder.Services.AddScoped<SeedingService>();
         builder.Services.AddScoped<SellerService>();
+        builder.Services.AddScoped<DepartmentService>();
 
         // Add services to the container.
         builder.Services.AddControllersWithViews();
