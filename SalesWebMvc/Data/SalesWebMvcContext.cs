5f9c5cbd3708e3be2ad7119050be6b3f239863ea
Bruno Donatoni <Brunodonatoni47@gmail.com>
2025-02-03 00:19:24 -0300

    Exception handling for delete, remake database

--- SalesWebMvc/Data/SalesWebMvcContext.cs
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -17,5 +17,17 @@ namespace SalesWebMvc.Data
         public DbSet<Department> Department { get; set; } = default!;
         public DbSet<Seller> Seller { get; set; }
         public DbSet<SalesRecord> SalesRecord { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Department>()
+            .HasMany(p => p.Sellers)
+            .WithOne(t => t.Department)
+            .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Seller>()
+            .HasMany(p => p.Sales)
+            .WithOne(t => t.Seller)
+            .OnDelete(DeleteBehavior.Restrict);
+        }
     }
 }
---
+++ SalesWebMvc/Services/Exceptions/IntegrityException.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore.SqlServer.Query.Internal;
+
+namespace SalesWebMvc.Services.Exceptions
+{
+    public class IntegrityException : ApplicationException
+    {
+
+        public IntegrityException(string message) : base(message) { }
+    }
+}
